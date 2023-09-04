1f7dd415480761cedba424e8ca1a4b686568f50f
Brenno <brennofruhauf@outlook.com>
2023-09-04 16:46:10 -0300

    fix bug

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export",
   skipTrailingSlashRedirect: true,
   distDir: "dist",
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@
 
 import Header from "@/components/basicos/Header"
 import "./globals.css"
-
-import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 import Footer from "@/components/basicos/Footer"
@@ -12,12 +10,6 @@ const poppins = Poppins({
   weight: ["100", "200", "400", "600", "700", "800", "900"],
   subsets: ["latin"],
 })
-
-export const metadata: Metadata = {
-  title: "Alex Tristão",
-  description:
-    "Inovação, inclusão e parceria para o crescimento e expansão do IF Goiano Campus Catalão",
-}
 
 export default function RootLayout({
   children,
