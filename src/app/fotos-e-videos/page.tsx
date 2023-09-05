8ee1f9e51a88f250808ea440abb94c47ec2cd261
Brenno <brennofruhauf@outlook.com>
2023-09-05 13:48:07 -0300

    fix image on modal

--- src/app/fotos-e-videos/page.tsx
+++ src/app/fotos-e-videos/page.tsx
@@ -28,42 +28,42 @@ export default function FotosEVideos() {
       <CardFoto
         url="/assets/images/fotos/DSC01291.jpg"
         alt="Professor Alex de terno em pose séria"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01291-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01293.jpg"
         alt="Professor Alex de terno em pose séria sorrindo"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01293-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01301.jpg"
         alt="Professor Alex de blusa azul sorrindo cruzando os braços"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01301-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01309.jpg"
         alt="Professor Alex de blusa azul sentado e sorrindo"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01309-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01312.jpg"
         alt="Professor Alex de blusa azul sentado mostrando os polegares"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01312-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01313.jpg"
         alt="Professor Alex de blusa verde com a mão no peito"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01313-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01321.jpg"
         alt="Professor Alex de blusa verde fazendo um 'V' com a mão"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01321-FULL.jpg"
       />
       <CardFoto
         url="/assets/images/fotos/DSC01325.jpg"
         alt="Professor Alex de blusa verde sentado em um banco"
-        urlFull="/assets/images/fotos/DSC01283-FULL.jpg"
+        urlFull="/assets/images/fotos/DSC01325-FULL.jpg"
       />
     </div>
   )
